319d38ccafe47c3957fab621dc9a63c69e40be73
mhamadelitawi <mhamadelitawi@gmail.com>
2021-12-02 20:09:57 +0200

    clean

---
+++ src/main/java/com/mhamadelitawi/peopleFlow/PeopleFlowApplication.java
@@ -0,0 +1,13 @@
+package com.mhamadelitawi.peopleFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhamadelitawi/peopleFlow/PeopleFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhamadelitawi.peopleFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
